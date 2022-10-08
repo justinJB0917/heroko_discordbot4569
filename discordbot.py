02088c5c39743aa3fe5d22ba869da96de384199b
justinJB0917 <96678491+justinJB0917@users.noreply.github.com>
2022-10-08 15:57:58 +0800

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -33,6 +33,16 @@ async def on_message(message):
         if translator.detect(content).lang == SRCLanguage or SRCLanguage == '':
             remessage = translator.translate(content, dest='zh-tw').text
             await message.reply(remessage) 
+            
+@client.event
+#當有訊息時
+async def on_message(message):
+    #排除自己的訊息，避免陷入無限循環
+    if message.author == client.user:
+        return
+    #如果包含 ping，機器人回傳 pong
+    if message.content == 'ping':
+        await message.channel.send('pong')            
 
 # Bot起動
 client.run(TOKEN)

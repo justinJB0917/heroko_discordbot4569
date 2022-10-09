2d3d356a980af3a96fbc451a2a775a5ea15ad278
justinJB0917 <96678491+justinJB0917@users.noreply.github.com>
2022-10-09 20:02:30 +0800

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -37,7 +37,7 @@ async def on_message(message):
         await message.delete()
 
         await message.channel.send(tmp[1])
-        print(tmp[1])
+        print(client.user, '發送了', tmp[1])
 
     if message.content == 'GG人':
         await message.delete()

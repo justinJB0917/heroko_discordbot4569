6309b5bcd2a7d8b3df493560a0120ad7fcfce657
justinJB0917 <96678491+justinJB0917@users.noreply.github.com>
2022-10-10 01:29:19 +0800

    Add files via upload

--- discordbot.py
+++ discordbot.py
@@ -1,10 +1,12 @@
 from distutils.command.clean import clean
 from email import message, message_from_binary_file
+from pydoc import cli
 from pyexpat.errors import messages
 from telnetlib import STATUS
 import discord
 import os
 from pprint import pprint
+import time
 
 TOKEN = os.environ['TOKEN']
 SRCLanguage=os.environ['SRC']
@@ -15,7 +17,7 @@ client = discord.Client()
 
 @client.event
 async def on_ready():
-    print('成功登入 登入身份：', client.user)
+    print('成功登入')
     game = discord.Game('Twitch 並觀賞Namin的直播')
     await client.change_presence(status=discord.Status.idle, activity=game)
 
@@ -37,7 +39,7 @@ async def on_message(message):
         await message.delete()
 
         await message.channel.send(tmp[1])
-        print(client.user, '發送了', tmp[1],)
+        print(client.user, '發送了', tmp[1])
 
     if message.content == 'GG人':
         await message.delete()
@@ -52,6 +54,28 @@ async def on_message(message):
         await message.delete()
         await message.channel.send("<你已遭到 管理員Relaxing234的永久禁言>")
         print(client.user, '撤回了一則訊息', '內容：','(這裡的觀眾都是機器人嗎)')
+    if message.content == '奇怪的叡':
+        await message.channel.send("觸發彩蛋 送你一張照片:)  https://ibb.co/rsHSw7F")
+        print(client.user, '觸發了1號彩蛋')
+    if message.content == '中二丁丁':
+        await message.channel.send("觸發彩蛋 送你一張照片:)  https://ibb.co/GPsnS4N")
+        print(client.user, '觸發了4號彩蛋')
+    if message.content == '顏利窘':
+        await message.channel.send("觸發彩蛋 送你一張照片:)  https://ibb.co/1fH2p9C")
+        print(client.user, '觸發了14號彩蛋')
+    if message.content == '綠藻頭':
+        await message.channel.send("觸發彩蛋 送你一張照片:)  https://ibb.co/9wH4NJC")
+        print(client.user, '觸發了25號彩蛋')
+    if message.content == '偏笨':
+        await message.channel.send("觸發彩蛋 送你一張照片:)  https://ibb.co/YkHqS7J 弄你我可能會被踢出去:D")
+        print(client.user, '觸發了20號彩蛋')
+    if message.content == 'Aidan Lu':
+        await message.channel.send("觸發彩蛋 送你一張照片:)  https://ibb.co/Cm0vZKW")
+        print(client.user, '觸發了10號彩蛋')
+    if message.content == '禿頭怪人':
+        await message.channel.send("觸發彩蛋 送你一張照片:)  https://ibb.co/k8T7sBb")
+        print(client.user, '觸發了26號彩蛋')
+    
 
     
 
